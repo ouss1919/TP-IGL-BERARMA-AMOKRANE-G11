4e8f6f90b015cb493cd5ad8ae2fc4f792db1a2e0
oussama <fo_berarma@esi.dz>
2017-10-22 11:29:19 +0200

    auto merging.

--- src/VectorHelper.java
+++ src/VectorHelper.java
@@ -6,6 +6,11 @@ import static sun.security.x509.CertificateVersion.V1;
  * Created by DELL on 14/10/2017.
  */
 public class VectorHelper {
+
+    void VectorHelper() {
+
+    }
+
     /**
      *  inverse les éléments d'un tableau
      *
@@ -78,6 +83,76 @@ public class VectorHelper {
         }catch(Exception e){
             System.out.print("DIFFERENT SIZE EXCEPTION");
         }
+        return res;
+    }
+
+
+    /**
+     *
+     * La méthode de triage d'un vecteur
+     *
+     * @param tableau
+     *          Contient le vecteur a triée.
+     * @return
+     *          Le tableau triée.
+     */
+
+    public void triRapide(ArrayList<Integer> tableau)
+    {
+        int longueur=tableau.size();
+        triRapide(tableau,0,longueur-1);
+    }
+
+    public static int partition(ArrayList<Integer> tableau, int deb, int fin)
+    {
+        int compt=deb;
+        int pivot=tableau.get(deb);
+
+        for(int i=deb+1;i<=fin;i++)
+        {
+            if (tableau.get(i)<pivot)
+            {
+                compt++;
+                echanger(tableau,compt,i);
+            }
+        }
+        echanger(tableau,deb,compt);
+        return(compt);
+    }
+
+    public static void echanger(ArrayList<Integer> tableau, int deb, int compt) {
+        int a = tableau.get(compt);
+        tableau.set(compt, tableau.get(deb));
+        tableau.set(deb, a);
+    }
+
+    public static void triRapide(ArrayList<Integer> tableau, int deb, int fin)
+    {
+        if(deb<fin)
+        {
+            int positionPivot=partition(tableau,deb,fin);
+            triRapide(tableau,deb,positionPivot-1);
+            triRapide(tableau,positionPivot+1,fin);
+        }
+    }
+
+    /**
+     * cherche le Max et le Min d'un tableau
+     * @param V //
+     * @return un tableau qui contient le max ensuite le min
+     */
+    ArrayList MaxMinVect (ArrayList V){
+        ArrayList res = new ArrayList();
+        int min ;
+        int max ;
+        min = (int) V.get(0);
+        max = (int) V.get(0);
+        for (int i=1;i<V.size();i++){
+            if ( (int) V.get(i) < min) min = (int) V.get(i);
+            if ( (int) V.get(i) > max) max = (int) V.get(i);
+        }
+        res.add(max);
+        res.add(min);
         return res;
     }
 }
