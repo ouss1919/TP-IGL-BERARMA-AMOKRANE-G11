830bd7b6e9fb5c0862f275f6ca223a8740c34a0f
oussama <fo_berarma@esi.dz>
2017-10-22 13:33:25 +0200

    add exception test.

--- src/VectorHelperTest.java
+++ src/VectorHelperTest.java
@@ -77,7 +77,9 @@ public class VectorHelperTest extends TestCase {
         t.add(2);
         t.add(123);
         t.add(-52);
+
         ArrayList<Integer> E=new ArrayList<>();
+
         E.add(-9);
         E.add(0);
         E.add(78);
@@ -90,5 +92,26 @@ public class VectorHelperTest extends TestCase {
         }
     }
 
+    public void testSommeDeuxVectException() throws Exception {
+        VectorHelper v=new VectorHelper();
+        ArrayList<Integer> t=new ArrayList<>();
+        t.add(-9);
+        t.add(0);
+        t.add(78);
+        t.add(2);
+        t.add(123);
+        t.add(-52);
+        t.add(30);
+
+        ArrayList<Integer> E=new ArrayList<>();
+
+        E.add(-9);
+        E.add(0);
+        E.add(78);
+        E.add(2);
+        E.add(123);
+        E.add(-52);
+        v.SommeDeuxVect(t, E);
+    }
 
 }
